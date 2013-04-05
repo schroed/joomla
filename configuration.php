3a359032e22d074d99e5931f38f733ae4e052874
Mathias Schröder <mathias@schroeder-info.de>
2013-04-05 13:20:10 +0200

    the beginning

---
+++ configuration.php
@@ -0,0 +1,65 @@
+<?php
+$_SERVER['DOCUMENT_ROOT'] = "/mnt/rid/88/56/53648856/htdocs/joomla_01";
+
+class JConfig {
+	public $offline = '0';
+	public $offline_message = 'This site is down for maintenance.<br /> Please check back again soon.';
+	public $display_offline_message = '1';
+	public $offline_image = '';
+	public $sitename = 'Walddörfer SV 2.Herren';
+	public $captcha = '0';
+	public $editor = 'tinymce';
+	public $list_limit = '20';
+	public $access = '1';
+	public $debug = '0';
+	public $debug_lang = '0';
+	public $dbtype = 'mysql';
+	public $host = 'rdbms.strato.de:';
+	public $user = 'U1296024';
+	public $password = 'XnTQFinSoaHxBdfjf';
+	public $db = 'DB1296024';
+	public $dbprefix = 'jos_';
+	public $live_site = '';
+	public $secret = 'spDNN5rBKCDE2lQh';
+	public $gzip = '0';
+	public $force_ssl = 0;                                          // Force areas of the site to be SSL ONLY.  0 = None, 1 = Administrator, 2 = Both Site and Administrator
+	public $error_reporting = '-1';
+	public $helpurl = 'http://help.joomla.org/proxy/index.php?option=com_help&amp;keyref=Help{major}{minor}:{keyref}';
+	public $ftp_host = '127.0.0.1';
+	public $ftp_port = '21';
+	public $ftp_user = '';
+	public $ftp_pass = '';
+	public $ftp_root = '';
+	public $ftp_enable = '0';
+	public $offset = 'UTC';
+	public $offset_user = 'UTC';
+	public $mailer = 'mail';
+	public $mailfrom = 'mathias@schroeder-info.de';
+	public $fromname = 'Walddörfer SV 2.Herren';
+	public $sendmail = '/usr/sbin/sendmail';
+	public $smtpport = '';
+	public $smtphost = '';
+	public $smtpauth = '0';
+	public $smtpuser = '';
+	public $smtppass = '';
+	public $smtpsecure = 'none';
+	public $caching = '0';
+	public $cache_handler = 'file';
+	public $cachetime = '15';
+	public $MetaDesc = '';
+	public $MetaKeys = '';
+	public $MetaTitle = '1';
+	public $MetaAuthor = '1';
+	public $sef = '1';
+	public $sef_rewrite = '0';
+	public $sef_suffix = '0';
+	public $unicodeslugs = '0';
+	public $feed_limit = '10';
+	public $log_path = '/mnt/rid/88/56/53648856/htdocs/joomla_01/logs';
+	public $tmp_path = '/mnt/rid/88/56/53648856/htdocs/joomla_01/tmp';
+	public $lifetime = '15';
+	public $session_handler = 'database';
+
+}
+
+?>
